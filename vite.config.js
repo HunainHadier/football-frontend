4a615853f46e3d6010c97a16e3f7a4dc6adc31a1
root <root@srv1136506.hstgr.cloud>
2025-12-09 07:28:44 +0000

    Ammar push

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,20 @@
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+//export default defineConfig({
+ // plugins: [react()],
+  //resolve: {
+    //alias: {
+      //'@/components': '/src/components',
+      //'@/utils': '/src/utils',
+      //'@/hooks': '/src/hooks',
+    //},
+  //},
+//})
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -11,4 +28,10 @@ export default defineConfig({
       '@/hooks': '/src/hooks',
     },
   },
+  server: {
+    host: true, // allow external access
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['sportsassessor.com', 'localhost', '127.0.0.1'],
+  }
 })
